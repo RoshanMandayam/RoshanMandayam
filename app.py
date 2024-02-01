eed0ee5b3a0dd0f3be969e098ce2618dfe84e1d2
Roshan Mandayam <mandayam.roshan@gmail.com>
2024-02-01 11:44:53 -0800

    Added basic JS functionality

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ def index():
 
 @app.route("/hello/<string:name>/")
 def hello(name):
-    return render_template(
-        'test.html',name=name)
+    return name
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
