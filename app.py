6130a348f6c9a690df98b7c915aa1f588299e1ad
Roshan Mandayam <mandayam.roshan@gmail.com>
2024-02-01 16:15:47 -0800

    Added a csv with list of US cities and coordinates for python backend to access. Added the ability for maps to pinpoint on the exact area that the city is in. Updated UI buttons/layout/map format. Now, when generate is clicked, it redirects to a different page in order to set the new variables in Flask. This will be changed in a later update.

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+import pandas as pd
+import random
+df = pd.read_csv('Data/uscities.csv')
 
 app = Flask(__name__)
-
+cityOne = ""
+cityTwo = ""
+latOne = 0
+latTwo = 0
+lonOne =0
+lonTwo = 0
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/hello/<string:name>/")
-def hello(name):
-    return name
+'''@app.route("/generateLocOne")
+def generateLocOne():
+    cit1 = random.randint(0,31119)
+    cityOne = df["city"][cit1]
+    return render_template('index.html', cityOne =cityOne, cityTwo = cityTwo )
+
+@app.route("/generateLocTwo")
+def generateLocTwo():
+    cit2 = random.randint(0,31119)
+    cityTwo = df["city"][cit2]
+    return render_template('index.html', cityOne = cityOne , cityTwo = cityTwo )
+'''
+@app.route("/generateLocations")
+def generateLocations():
+    cit1 = random.randint(0,31119)
+    cityOne = df["city"][cit1]
+    cit2 = random.randint(0,31119)
+    cityTwo = df["city"][cit2]
+    latOne = float(df["lat"][cit1])
+    lonOne = float(df["lng"][cit1])
+    latTwo = float(df["lat"][cit2])
+    lonTwo = float(df["lng"][cit2])
+
+    return render_template('index.html', cityOne = cityOne , cityTwo = cityTwo, latOne =latOne, latTwo=latTwo,lonOne=lonOne,lonTwo=lonTwo  )
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
