7fcd53ab12bfb2717b247a0313c0ce2bac85b7bd
Roshan Mandayam <mandayam.roshan@gmail.com>
2024-02-21 19:36:48 -0800

    Added Database functionality usisng SQLite and sqlalchemy library. Now the top 10 leaderboard is shown when you ssubmit your  guess.

--- README.md
+++ README.md
@@ -12,3 +12,44 @@ Import random
 Install simplejson
 import simplejson, urllib
 install googlemaps
+
+datetime==2023.4
+Client google maps??
+Flask/ssqlalchemy
+pip install --only-binary :all: greenlet
+pip install --only-binary :all: Flask-SQLAlchemy
+
+
+
+
+List of useful commands:
+
+/Users/roshanmandayam/are-we-there-yet
+docker run -v /host/directory:/container/directory image_name
+
+
+
+
+docker exec quizzical_agnesi pwd
+
+
+
+docker build -t roshanmark3 .
+
+
+docker run -p 8080:8080 -v /Users/roshanmandayam/are-we-there-yet:/app roshanmark3
+
+
+docker run -p 8080:8080 roshanmark3
+
+
+
+
+
+python
+>>> from app import db
+>>> db.create_all()
+>>> exit()
+
+
+docker exec -it quizzical_agnesi python3
--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from googlemaps import Client
 from flask import jsonify
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 df = pd.read_csv('Data/uscities.csv')
 #print(df.shape)
@@ -19,46 +20,33 @@ import urllib.request
 
 
 app = Flask(__name__)
+ma = Marshmallow(app)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///leaderboard.db'
+
+db = SQLAlchemy(app)
+
+class leaderBoard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+    date_added = db.Column(db.DateTime,default =  datetime.utcnow)
+
+    def __repr__(self):
+        return '<score %r>' % self.score
+
 app.secret_key = "abcde2o38cniuwc"
-'''cityOne = ""
-cityTwo = ""
-latOne = 0
-latTwo = 0
-lonOne =0
-lonTwo = 0
-time = 0
-'''
+
+
+
 @app.route("/", methods = ['GET','POST'])
 def index():
-    
-    '''if request.method == 'GET':
-        session['cit1'] = random.randint(0,31119)
-        session['cityOne'] = df["city"][session['cit1']]
-        session['cit2'] = random.randint(0,31119)
-        session['cityTwo'] = df["city"][session['cit2']]
-        session['latOne'] = float(df["lat"][session['cit1']])
-        session['lonOne'] = float(df["lng"][session['cit1']])
-        session['latTwo'] = float(df["lat"][session['cit2']])
-        session['lonTwo'] = float(df["lng"][session['cit2']])
-        return render_template('index.html', cityOne = session['cityOne'] , cityTwo = session['cityTwo'], latOne =session['latOne'], latTwo=session['latTwo'],lonOne=session['lonOne'],lonTwo=session['lonTwo']  )
-'''
-    # session['cityOne'] = ""
-    # session['cit1'] = ""
-    # session['cityOne'] = ""
-    # session['cit2'] = ""
-    # session['cityTwo'] =""
-    # session['latOne'] = ""
-    # session['lonOne'] = ""
-    # session['latTwo'] = ""
-    # session['lonTwo'] = ""
-    return render_template('index.html', cityOne = session['cityOne'] , cityTwo = session['cityTwo'], latOne =session['latOne'], latTwo=session['latTwo'],lonOne=session['lonOne'],lonTwo=session['lonTwo']  )
-
-    #return render_template('index.html')
+    session['LeaderBoardData'] = ''
+    return render_template('index.html', position = session['LeaderBoardData'], cityOne = session['cityOne'] , cityTwo = session['cityTwo'], latOne =session['latOne'], latTwo=session['latTwo'],lonOne=session['lonOne'],lonTwo=session['lonTwo']  )
 
 
-
-@app.route('/create_file', methods=['POST','GET'])
-def create_file():
+@app.route('/genLocations', methods=['POST','GET'])
+def genLocations():
+    session['LeaderBoardData'] = ''
     session['cit1'] = random.randint(0,31119)
     session['cityOne'] = df["city"][session['cit1']]
     session['cit2'] = random.randint(0,31119)
@@ -73,10 +61,6 @@ def create_file():
                       "Location 2 Lon": session['lonTwo'],
                       "Location 1 Lat": session['latOne'],
                       "Location 1 Lon": session['lonOne']})
-    if request.method == 'GET':
-        
-        return render_template('index.html', cityOne = session['cityOne'] , cityTwo = session['cityTwo'], latOne =session['latOne'], latTwo=session['latTwo'],lonOne=session['lonOne'],lonTwo=session['lonTwo']  )
-
 
 
 @app.route("/submitGuess", methods = ['POST','GET'])
@@ -87,51 +71,36 @@ def submitGuess():
     
     gmaps = googlemaps.Client(key=session['key'])
 
-    #session['orig'] = str(session['latOne'])+", "+str(session['lonOne'])
-    #session['dest'] = str(session['latTwo'])+", "+str(session['lonTwo'])
     session['orig'] = session['latOne'],session['lonOne']
     session['dest'] = session['latTwo'],session['lonTwo']
 
-    #return session['orig']
     now = datetime.now()
     directions_result = gmaps.directions(session['orig'],
                                      session['dest'],
                                      mode="walking",
                                      departure_time=now
                                     )
-    #return directions_result[0]['legs'][0]['distance']['text']
-    #return directions_result[0]['legs'][0]['duration']['text']
-    #print(directions_result[0]['legs'][0]['duration']['text'])
-    #return directions_result[0]['legs'][0]['distance']['text']
-   
-    #print(cityOne)
-    #return session['cityOne']
-    
-    '''session['url'] = "https://maps.googleapis.com/maps/api/distancematrix/json?key={0}&origins={1}&destinations={2}&mode=driving&language=en-EN&sensor=false".format(session['key'],(session['orig']),(session['dest']))
-    session['url'] = session['url'].replace("%20", "")
-    session['url'] = session['url'].replace(" ", "")
-    session['result']= simplejson.load(urllib.request.urlopen(session['url']))
-    #return (session['url'])
-    return session['result']['rows'][0]['elements'][0]
-    session['time'] = session['result']['rows'][0]['elements'][0]['duration']['text']
-    '''
-    #if directions_result[0]['legs'][0]['duration']['value'] == None:
-     #   return "not possible to walk there,regenerate"
-    
-    #try:
+ 
+
     session['time'] = directions_result[0]['legs'][0]['duration']['value']
-    #except ValueError:
-     #   return "It is not possible to walk to one of the above cities, please generate again!"
-    #return render_template('index.html', cityOne = session['cityOne'] , cityTwo = session['cityTwo'], latOne =session['latOne'], latTwo=session['latTwo'],lonOne=session['lonOne'],lonTwo=session['lonTwo'], time=session['time']  )
-    #try:
+    
     score = session['time'] - int(request.form['guess'])
     if score < 0:
         score = session['time']
+
+    attempt = leaderBoard(score = score)
+    db.session.add(attempt)
+    db.session.commit()
+    board = leaderBoard.query.order_by(leaderBoard.score).limit(10)
+    #board = jsonify(board)
+    #session['LeaderBoardData'] = leaderBoard.query.order_by(leaderBoard.date_added)
     return jsonify({"time": "Correct Answer: " + str(session['time']) + " seconds",
-                    "myScore": "Your Score: " + str(score) + " points!" })
-    #return render_template('index.html',cityOne = cityOne , cityTwo = cityTwo)
-    #except:
-     #   raise Exception( "Please enter your ")
+                    "myScore": "Your Score: " + str(score) + " points!", "data": [{ "id": str(user.id) + " ;  " , "score": str(user.score) + " points ", "date":str(user.date_added) + " ; "} for user in board] }
+                    )
+    #.update(
+     #                   { "data": [{ "id": user.id, "score": user.score} for user in board] }
+      #                  )'''
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
 
